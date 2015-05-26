353a9bb326e6c813ccc8f5b952fb0d8ecd8fc95e
David McCauley <xf57212@umbc.edu>
2015-05-26 16:06:50 -0400

    Removed towny version checked. Created a conflict and would always auto disable on newer versions.

--- src/backcab/TownyNameUpdater/TownyNameUpdater.java
+++ src/backcab/TownyNameUpdater/TownyNameUpdater.java
@@ -1,15 +1,12 @@
 package backcab.TownyNameUpdater;
 
 import java.util.UUID;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.palmergames.bukkit.towny.object.Resident;
@@ -21,26 +18,7 @@ public class TownyNameUpdater extends JavaPlugin implements Listener{
 	private PluginFile map;
 	
 	public void onEnable(){
-		final Logger logger = this.getLogger();
-		
-		Bukkit.getScheduler().runTaskLater(this, new Runnable(){
-			public void run(){
-				if(!Bukkit.getPluginManager().isPluginEnabled("Towny")){
-					logger.log(Level.SEVERE, "Towny is not enabled. Disabling this plugin.");
-					Bukkit.getPluginManager().disablePlugin(Bukkit.getPluginManager().getPlugin("TownyNameUpdater"));
-					return;
-				}
-				
-				String minVersion = "0.89.2.2";
-				
-				Plugin towny = Bukkit.getPluginManager().getPlugin("Towny");
-				
-				if(minVersion.compareTo(towny.getDescription().getVersion().replaceAll("v", "")) > 0){
-					logger.log(Level.SEVERE, "Towny version " + minVersion + " or higher required. Disabling this plugin");
-				}
-			}
-		}, 0);
-		
+
 		map = new PluginFile(this, "playermap", false);
 		
 		Bukkit.getPluginManager().registerEvents(this, this);
