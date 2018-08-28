6ade5a537177b5b44d59d5fecdf375549c4fe130
Articdive <articdive@gmail.com>
2018-08-28 20:56:21 +0200

    Removed Enum for 1 Node, Node wasn't necessary anyway, simplified code.

--- src/com/palmergames/townynameupdater/TownyNameUpdaterConfiguration.java
+++ src/com/palmergames/townynameupdater/TownyNameUpdaterConfiguration.java
@@ -6,7 +6,7 @@ import com.palmergames.util.FileMgmt;
 import java.io.File;
 
 public class TownyNameUpdaterConfiguration {
-	private static CommentedConfiguration config, newConfig;
+	private static CommentedConfiguration config;
 
 	public static void loadConfig(String filepath) {
 
@@ -17,48 +17,17 @@ public class TownyNameUpdaterConfiguration {
 		if (!config.load())
 			System.out.print("Failed to load Config!");
 
-		setDefaults(file);
-
-		config.save();
-	}
-
-	private static void addComment(String root, String... comments) {
-
-		newConfig.addComment(root.toLowerCase(), comments);
-	}
-
-	/**
-	 * Builds a new config reading old config data.
-	 */
-	private static void setDefaults(File file) {
-
-		newConfig = new CommentedConfiguration(file);
+		CommentedConfiguration newConfig = new CommentedConfiguration(file);
 		newConfig.load();
 
-		for (ConfigurationNodes root : ConfigurationNodes.values()) {
-			if (root.getComments().length > 0) {
-				addComment(root.getRoot(), root.getComments());
-			}
-
-			setNewProperty(root.getRoot(), config.get(root.getRoot().toLowerCase()) != null ? config.get(root.getRoot().toLowerCase()) : root.getDefault());
-		}
-
 		config = newConfig;
-		newConfig = null;
+
+		config.save();
 	}
 
 	private static void setProperty(String root, Object value) {
 
 		config.set(root.toLowerCase(), value.toString());
-	}
-
-	private static void setNewProperty(String root, Object value) {
-
-		if (value == null) {
-			// System.out.print("value is null for " + root.toLowerCase());
-			value = "";
-		}
-		newConfig.set(root.toLowerCase(), value.toString());
 	}
 
 
@@ -75,60 +44,4 @@ public class TownyNameUpdaterConfiguration {
 		setProperty(root, value);
 		config.save();
 	}
-}
-
-enum ConfigurationNodes {
-	MAIN(
-			"uuid",
-			"name",
-			"# This is where Towny stores all of the uuid to username mappings.",
-			"# DO NOT TOUCH THESE UNLESS YOU KNOW WHAT YOU ARE DOING!"
-	);
-	private final String Root;
-	private final String Default;
-	private String[] comments;
-
-	ConfigurationNodes(String root, String def, String... comments) {
-
-		this.Root = root;
-		this.Default = def;
-		this.comments = comments;
-	}
-
-	/**
-	 * Retrieves the root for a config option
-	 *
-	 * @return The root for a config option
-	 */
-	public String getRoot() {
-
-		return Root;
-	}
-
-	/**
-	 * Retrieves the default value for a config path
-	 *
-	 * @return The default value for a config path
-	 */
-	public String getDefault() {
-
-		return Default;
-	}
-
-	/**
-	 * Retrieves the comment for a config path
-	 *
-	 * @return The comments for a config path
-	 */
-	public String[] getComments() {
-
-		if (comments != null) {
-			return comments;
-		}
-
-		String[] comments = new String[1];
-		comments[0] = "";
-		return comments;
-	}
-
 }
