355b0d35042228f70e1552860b47694fb876d4bb
LlmDl <LlmDlio@gmail.com>
2020-04-25 15:50:55 -0500

    TownyNameUpdater 5.0 (#3)
    - Changes PlayerLoginEvent for AsyncPlayerPreLoginEvent.
    - Adds logging output when a name change is detected.
    - Updates Towny version in pom.xml.

--- src/main/com/palmergames/townynameupdater/TownyNameUpdater.java
+++ src/main/com/palmergames/townynameupdater/TownyNameUpdater.java
@@ -6,7 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -23,9 +23,9 @@ public class TownyNameUpdater extends JavaPlugin implements Listener {
 	}
 
 	@EventHandler(priority = EventPriority.LOW)
-	public void onJoin(PlayerLoginEvent event) {
-		String currName = event.getPlayer().getName();
-		UUID id = event.getPlayer().getUniqueId();
+	public void onJoin(AsyncPlayerPreLoginEvent event) {
+		String currName = event.getName();
+		UUID id = event.getUniqueId();
 		String oldName = TownyNameUpdaterConfiguration.getString(id.toString());
 
 		if (oldName.isEmpty()) {
@@ -34,6 +34,8 @@ public class TownyNameUpdater extends JavaPlugin implements Listener {
 		}
 
 		if (!oldName.equals(currName)) {
+			System.out.println("[TownyNameUpdater] Player name change detected! " + oldName + " has changed their name to " + currName + ". Updating database.");
+
 			try {
 				Resident r = TownyUniverse.getInstance().getDataSource().getResident(oldName);
 				TownyUniverse.getInstance().getDataSource().renamePlayer(r, currName);
